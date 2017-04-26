207048a49fec686cc76a49ed9b686f84e0c8d8ad
joonhyongcho <joonhyongcho@gmail.com>
2017-04-26 10:26:29 -0400

    Change gitignore and modify app.py to include settings

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 App = Flask(__name__, template_folder='static')
-
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 # decorator function
 @App.route("/")
